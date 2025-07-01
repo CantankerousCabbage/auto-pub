0600b6809e01ce75084755349a25021d66200f18
Joseph Warren <josipwarren@gmail.com>
2025-07-02 09:13:00 +1000

    devpublish base implementation

--- src/cli/actions/devPublish.ts
+++ src/cli/actions/devPublish.ts
@@ -1,8 +1,19 @@
 import { ActionInput } from '@/types/actionInput.js';
 import { Action } from './action.js';
-import { logError, logInfo } from '@/utils/logging.js';
+import { logError, logInfo, logDebug } from '@/utils/logging.js';
 import { runScript } from '@/scripthandler.js';
 import { Config } from '@/types/config.types.js';
+  import axios from 'axios';
+import { spawnSync } from 'child_process';
+import * as fs from 'fs';
+import * as path from 'path';
+import { IG_PUBLISHER_JAR } from '@/constants/constant.js';
+import { FHIR_ORG_URL } from '@/constants/constant.js';
+import { generateInternalPath, generateProcessPath } from '@/utils/utilPath.js';
+import { httpClient } from '@/utils/utilWebClient.js';
+// const publisherJar = 'publisher.jar';
+// const inputCachePath = './input-cache';
+// const publisherPath = path.join(inputCachePath, publisherJar);
 
 class DevPublish extends Action {
   constructor() {
@@ -19,19 +30,48 @@ class DevPublish extends Action {
 
   async execute(): Promise<boolean> {
     let success = true;
-    logInfo(`Executing action for ${this.getActionName()}`);
 
-    // const target = internalPath('../scripts/test.sh');
-    const target = '_genonce.sh';
+  logDebug('Checking internet connection...');
 
-    try {
-      const stdout = await runScript(target);
-      logInfo(`Script output: ${stdout}`);
+  let txOption = '';
+  try {
+    await httpClient.get('https://tx.fhir.org');
+    logDebug('Online');
+  } catch {
+    logDebug('Offline');
+    txOption = '-tx n/a';
+  }
 
-    } catch (error) {
-        logError(`Error running script: ${error.message}`);
-        success = false;
+  console.log(txOption);
+
+  const publisherPath = generateInternalPath(IG_PUBLISHER_JAR);
+  const igPath = generateProcessPath('./');
+
+
+  if (fs.existsSync(publisherPath)) {
+    const args = [
+      '-Dfile.encoding=UTF-8',
+      '-jar',
+      publisherPath,
+      '-ig',
+      igPath,
+      ...txOption.split(' '),
+      ...process.argv.slice(2),
+    ];
+
+    //Set IO variables to inherit from current process
+    const result = spawnSync('java', args, { stdio: 'inherit' });
+
+    if (result.error) {
+      console.error('Error running IG Publisher:', result.error);
+      process.exit(1);
     }
+  } else {
+    console.error('IG Publisher NOT FOUND in input-cache. Please run _updatePublisher. Aborting...');
+    process.exit(1);
+  }
+
+
 
     return Promise.resolve(success);
   }
--- src/cli/constants/constant.ts
+++ src/cli/constants/constant.ts
@@ -2,3 +2,4 @@ export const IG_PUBLISHER_DOWNLOAD_URL = "https://github.com/HL7/fhir-ig-publish
 // export const IG_PUBLISHER_VERSION_URL = "https://api.github.com/repos/HL7/fhir-ig-publisher/tags | jq -r '.[1].name";
 export const IG_PUBLISHER_VERSION_URL = "https://api.github.com/repos/HL7/fhir-ig-publisher/tags";
 export const IG_PUBLISHER_JAR = "publisher.jar";
+export const FHIR_ORG_URL = "https://tx.fhir.org";
